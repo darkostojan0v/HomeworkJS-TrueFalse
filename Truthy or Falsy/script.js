0bb15e5be898e16542663907ebf7853cd0719c4a
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-01-09 15:00:14 +0100

    Add files via upload

---
+++ Truthy or Falsy/script.js
@@ -0,0 +1,13 @@
+console.log('True or false');
+
+function trueOrFalse() {
+    let x = 1;
+
+    if ( 1 == '1') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(trueOrFalse());
